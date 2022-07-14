21ed18e1a41f63f4a709892cbb263bb7549899e0
JeongChan2 <jcstar55@gmail.com>
2022-07-14 14:57:32 +0900

    Update print.py
    Update learned contents

--- print.py
+++ print.py
@@ -36,3 +36,18 @@ print('%.5s' % ('nice'))
 print('%.5s' % ('pythonstudy'))
 print('%5s' % ('pythonstudy'))
 print('{:10.5}'.format('pythonstudy')) #공간은 10개가있는데 왼쪽부터 5개만 출력
+
+# %d
+print('%d %d' % (1,2))
+print('{} {}'.format(1,2))
+
+print('%4d' % (42))
+print('{:4d}'.format(42))
+
+# %f
+
+print('%f' % (3.141592111222))
+print('{:4f}'.format(3.141592111222))
+
+print('%06.2f'%(3.1415921221313)) #총 6자리 죽에 앞은 0으로 채워라
+print('{:06.2f}'.format(3.14159212323123))
